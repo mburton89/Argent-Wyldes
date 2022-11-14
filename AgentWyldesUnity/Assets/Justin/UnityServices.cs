6e490fe0782c643624f037d5d27657156a751f81
WUStudent <wustudent@webster.edu>
2022-11-14 17:02:22 -0600

    unity services dooesnt work

---
+++ AgentWyldesUnity/Assets/Justin/UnityServices.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+
+public class UnityServices : MonoBehaviour
+{
+
+    //private UnityTransport unityTransport;
+    private const int MaxPlayers = 7;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
